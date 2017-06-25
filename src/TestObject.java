99d5859ab96be8a11616576e8df7e38908b5c743
Joseph <joseph.a.gauthier@gmail.com>
2017-06-25 10:32:15 -0500

    NextCommit

--- src/TestObject.java
+++ src/TestObject.java
@@ -4,9 +4,14 @@ public class TestObject {
 	public TestObject() {
 		System.out.println("Created");
 		foo();
+		bar();
 	}
 	
 	public void foo() {
 		System.out.println("Next");
+	}
+	
+	public void bar() {
+		System.out.println("Then");
 	}
 }
