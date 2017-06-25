96fff2ada0100f7c082598d68064254631493c08
Joseph <joseph.a.gauthier@gmail.com>
2017-06-25 10:41:12 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public Main() {
 		TestObject to = new TestObject();
+		NextTestObject to2 = new NextTestObject();
 	}
 
 	public static void main(String[] args) {
---
+++ src/NextTestObject.java
@@ -0,0 +1,11 @@
+
+public class NextTestObject {
+	
+	public NextTestObject() {
+		myMethod();
+	}
+	
+	public void myMethod() {
+		System.out.println("Method");
+	}
+}
